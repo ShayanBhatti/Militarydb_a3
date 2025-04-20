16cd29c44b5884329d7339af3277fde36a5b7fc6
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-04-20 12:56:23 +0500

    it is working

--- MyMvcApp/obj/Debug/net9.0/MyMvcApp.AssemblyInfo.cs
+++ MyMvcApp/obj/Debug/net9.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cc2e517566fac02f5e8a877dadea71075c423ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394614befd23b4b733fe487284395da7384ead60")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
